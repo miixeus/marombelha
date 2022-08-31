22d96483127268dfb2edda5b305cac9ed5b46a57
mixeus <mixeuskaczala@gmail.com>
2022-08-31 18:15:49 -0300

    slider

---
+++ slick.js
@@ -0,0 +1,6 @@
+$('.carousel').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
--- slick.min.js
+++
@@ -1,7 +0,0 @@
-$('.carousel').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-  });
